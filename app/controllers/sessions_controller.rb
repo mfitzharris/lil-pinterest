4470bfa59bd626fd0cbc83f033bfc8e6f37a5a7f
mfitzharris <molly.fitzharris@gmail.com>
2017-02-19 17:49:28 -0600

    Controller logic and views for logging in and out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    @user
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      @errors = ["Matching record not found, please try again"]
+      render 'new'
+    end
+  end
+
+  def delete
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+end
